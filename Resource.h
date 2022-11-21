b7ce66fe5f085c0dfaaf0be4d9aa8d3a62ba021a
EjnarRaidriar <emanuil.cucos25@gmail.com>
2022-11-21 16:59:16 +0200

    add resource .h

---
+++ Resource.h
@@ -0,0 +1,55 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+#include<string>
+class Resource
+{
+private:
+    std::string name;
+    int durability;
+public:
+    //Construncotrs and Destructors
+    Resource();
+    Resource(std::string name, int durability);
+    ~Resource();
+    //Getters and Setters
+    void setName(std::string name);
+    void setDurablity(int durability);
+    std::string getName();
+    int getDurability();
+};
+
+Resource::Resource(std::string name, int durability)
+{
+    this->name = name;
+    this->durability = durability;
+}
+
+Resource::Resource()
+{
+}
+
+Resource::~Resource()
+{
+}
+//Getters and Setters
+void Resource::setName(std::string name)
+{
+    this->name = name;
+}
+
+void Resource::setDurablity(int durability)
+{
+    this->durability = durability;
+}
+
+int Resource::getDurability()
+{
+    return this->durability;
+}
+
+std::string Resource::getName()
+{
+    return this->name;
+}
+
+#endif
